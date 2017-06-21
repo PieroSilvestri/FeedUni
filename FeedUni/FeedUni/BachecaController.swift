fa9e596ed8895963bb4446a41fae396f3abda2e4
Unknown <scocchiello@gmail.com>
2017-06-21 16:46:00 +0200

    Push Sbuk

--- FeedUni/FeedUni/BachecaController.swift
+++ FeedUni/FeedUni/BachecaController.swift
@@ -1,27 +1,37 @@
 //
-//  LoginController.swift
+//  BachecaController.swift
 //  FeedUni
 //
-//  Created by Andrea Scocchi on 20/06/17.
+//  Created by Andrea Scocchi on 21/06/17.
 //  Copyright © 2017 Piero Silvestri. All rights reserved.
 //
 
 import UIKit
 
-class LoginController: UIViewController {
+class BachecaController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 0
+    }
     
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        return 0
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+
     /*
     // MARK: - Navigation
 
