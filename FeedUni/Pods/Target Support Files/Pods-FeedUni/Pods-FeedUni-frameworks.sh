ae32c68f066beb2d372215e73c46a5cb40316424
reginatojames <jamesrgnt@gmail.com>
2017-06-21 14:39:32 +0200

    realm

--- FeedUni/Pods/Target Support Files/Pods-FeedUni/Pods-FeedUni-frameworks.sh
+++ FeedUni/Pods/Target Support Files/Pods-FeedUni/Pods-FeedUni-frameworks.sh
@@ -89,10 +89,14 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/Realm/Realm.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/RealmSwift/RealmSwift.framework"
   install_framework "$BUILT_PRODUCTS_DIR/TabPageViewController/TabPageViewController.framework"
   install_framework "$BUILT_PRODUCTS_DIR/TimelineTableViewCell/TimelineTableViewCell.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/Realm/Realm.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/RealmSwift/RealmSwift.framework"
   install_framework "$BUILT_PRODUCTS_DIR/TabPageViewController/TabPageViewController.framework"
   install_framework "$BUILT_PRODUCTS_DIR/TimelineTableViewCell/TimelineTableViewCell.framework"
 fi
