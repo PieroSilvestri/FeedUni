ea6d9c9d60ffc82609a0e6bc98d46cb600778041
Unknown <scocchiello@gmail.com>
2017-06-21 14:32:57 +0200

    RISOLTISSIMA

--- FeedUni/FeedUni/Controller/LoginController.swift
+++ FeedUni/FeedUni/Controller/LoginController.swift
@@ -11,19 +11,9 @@ import UIKit
 class LoginController: UIViewController {
 
     
-    @IBOutlet weak var logoUni: UIImageView!
-
-    @IBOutlet weak var emailText: UITextField!
-    @IBOutlet weak var passwordText: UITextField!
-    
-    @IBOutlet weak var signInButton: UIButton!
-    @IBOutlet weak var facebookButton: UIButton!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        emailText.leftViewMode = UITextFieldViewMode.always
-        emailText.leftView = UIImageView(image: UIImage(named: "imageName"))
         // Do any additional setup after loading the view.
     }
     
