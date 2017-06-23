e04f5d6a014e8b5f00e2722d2d658ca38623f130
Giacomo Osso <tsam017@tecnicosuperiorekennedy.it>
2017-06-23 12:23:37 +0200

    update register
    aggiunto controllo se è connesso ad internet

--- FeedUni/FeedUni/RegisterController.swift
+++ FeedUni/FeedUni/RegisterController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SystemConfiguration
 
 class RegisterController: UIViewController {
 
@@ -25,6 +26,7 @@ class RegisterController: UIViewController {
     
    
     @IBAction func SignInPressed(_ sender: UIButton) {
+        if(isConnectedToNetwork()){
         if(!(validateEmail(enteredEmail: emailTextField.text!))){
             
             // create the alert
@@ -152,6 +154,20 @@ class RegisterController: UIViewController {
             self.present(alert, animated: true, completion: nil)
 
         }
+        }else{
+            // create the alert
+            let alert = UIAlertController(title: "Registrazione", message: "Non sei connesso ad internet", preferredStyle: UIAlertControllerStyle.alert)
+            
+            // add an action (button)
+            alert.addAction(UIAlertAction(title: "Riprova", style: UIAlertActionStyle.default, handler: { action in
+                return;
+            }))
+            
+            // show the alert
+            self.present(alert, animated: true, completion: nil)
+
+        }
+        
     }
 
     func getJsonFromUrl(page: Int){
@@ -211,7 +227,27 @@ class RegisterController: UIViewController {
         return activityIndicatorView
     }
     
-    
+    func isConnectedToNetwork() -> Bool {
+        
+        var zeroAddress = sockaddr_in(sin_len: 0, sin_family: 0, sin_port: 0, sin_addr: in_addr(s_addr: 0), sin_zero: (0, 0, 0, 0, 0, 0, 0, 0))
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+            }
+        }
+        
+        var flags = SCNetworkReachabilityFlags()
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+            return false
+        }
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        
+        return (isReachable && !needsConnection)
+        
+    }
     /*
     // MARK: - Navigation
 
