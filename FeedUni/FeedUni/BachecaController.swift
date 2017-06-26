46714c4e16eae5595562abc56c1642838106ae0d
Unknown <scocchiello@gmail.com>
2017-06-26 09:12:25 +0200

    Test

--- FeedUni/FeedUni/BachecaController.swift
+++ FeedUni/FeedUni/BachecaController.swift
@@ -8,9 +8,21 @@
 
 import UIKit
 
-class BachecaController: UIViewController {
+class BachecaController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 5
+    }
+
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! BachecaControllerTableViewCell
+        
+        return cell
+    }
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
 
