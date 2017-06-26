94cc35d55f194a16e87c030221ecbac3a9b7e472
Giacomo Osso <tsam017@tecnicosuperiorekennedy.it>
2017-06-26 16:46:27 +0200

    niente

--- FeedUni/FeedUni/NewsController.swift
+++ FeedUni/FeedUni/NewsController.swift
@@ -26,6 +26,7 @@ class NewsController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
         // Dispose of any resources that can be recreated.
     }
     
