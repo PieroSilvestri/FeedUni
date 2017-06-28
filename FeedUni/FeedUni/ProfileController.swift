3c4b27b229496db1967ca248c62f7ea1a22eac88
munsra <piero.silvestri93@gmail.com>
2017-06-28 15:55:47 +0200

    Dov'è Melillo? :(

---
+++ FeedUni/FeedUni/ProfileController.swift
@@ -0,0 +1,36 @@
+//
+//  ProfileController.swift
+//  FeedUni
+//
+//  Created by Piero Silvestri on 28/06/2017.
+//  Copyright © 2017 Piero Silvestri. All rights reserved.
+//
+
+import UIKit
+
+class ProfileController: UIViewController {
+    @IBOutlet weak var usernameLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
