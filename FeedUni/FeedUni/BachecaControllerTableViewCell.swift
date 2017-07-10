4306963cde7285a39af06e18ba99c9e3e3a5aacd
Unknown <scocchiello@gmail.com>
2017-07-10 21:13:15 +0200

    un push a caso

--- FeedUni/FeedUni/BachecaControllerTableViewCell.swift
+++ FeedUni/FeedUni/BachecaControllerTableViewCell.swift
@@ -14,7 +14,6 @@ class BachecaControllerTableViewCell: UITableViewCell {
     @IBOutlet weak var cellTitle: UILabel!
     @IBOutlet weak var cellPrice: UILabel!
     @IBOutlet weak var cellData: UILabel!
-    @IBOutlet weak var cellHeart: UIImageView!
 
     
     override func awakeFromNib() {
